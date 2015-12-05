6a28504a9798ee0c90301b4af977488b18ba18f5
Tan Bui <trungtanbui@gmail.com>
2015-12-05 12:03:36 +0700

    {print Demo ode-static}

---
+++ httpsvr.js
@@ -0,0 +1,12 @@
+var http = require('http');
+//https://www.npmjs.com/package/node-static
+var static = require('node-static');
+var file = new static.Server('./public');
+
+var svr = http.createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+});
+svr.listen(8080, 'localhost');
+console.log('Server running at http://localhost:8080/');
